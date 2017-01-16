9dac314fa037086ddc31a0f23eae37f6e4fe2220
eyupy <eyupy@EYMEL-PC>
2017-01-16 17:18:41 +0300

    Add project files.

---
+++ WebAPİİİ/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPİİİ.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public int Getulan()
+        {
+            return 2 + 2;
+        }
+    }
+}
